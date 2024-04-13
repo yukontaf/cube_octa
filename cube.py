83301a38fd9d84aca09da351ac0e712547475b40
Gleb Sokolov <98272104+yukontaf@users.noreply.github.com>
2024-04-13 05:33:39 +0300

    init

--- cube.py
+++ cube.py
@@ -1,7 +1,19 @@
+from cube import config
+
+config.base_path = '/cube-api'
+
+config.context_to_app_id = lambda ctx: ctx['securityContext']['tenant_id']
+
+def rewrite(query, ctx):
+  query['measures'].append('orders.count')
+  return query
+
+config.query_rewrite = rewrite
+
 from cube import TemplateContext
 from cube_dbt import Dbt
 
-dbt = Dbt.from_file('./manifest.json')
+dbt = Dbt.from_file('manifest.json')
 
 template = TemplateContext()
 
