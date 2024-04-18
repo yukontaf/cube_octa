d93962a35292f90bef05f5a3318e86b65aa89553
Gleb Sokolov <yukontaf@gmail.com>
2024-04-18 22:20:36 +0300

    minor changes

--- model/cubes/push_problem/failed_pushes_users.js
+++ model/cubes/push_problem/failed_pushes_users.js
@@ -19,9 +19,6 @@ cube(`failed_pushes_users`, {
       1,
       2 `,
   preAggregations: {
-    // originalSql: {
-    //   type: `originalSql`
-    // },
     failed_users: {
       dimensions: [
         failed_pushes_users.appsflyerId,
@@ -36,6 +33,20 @@ cube(`failed_pushes_users`, {
         }
       }
     },
+    failed_pushes: {
+      type: `rollup`,
+      dimensions: [
+        failed_pushes_users.campaignId,
+        failed_pushes_users.campaignPolicy,
+      ],
+      measures: [
+        failed_pushes_users.count,
+        failed_pushes_users.distinct_users
+      ],
+      timeDimension: failed_pushes_users.timestamp,
+      granularity: `day`,
+      partitionGranularity: `week`
+    }
   },
   joins: {
     int_bloomreach_events_enhanced: {
@@ -47,28 +58,36 @@ cube(`failed_pushes_users`, {
       sql: `${CUBE}.appsflyerId = ${appsflyer_uninstall_events_report}.appsflyer_id`
     },
   },
-    measures: {
-      count: {
-        type: `count`
-      }
+  measures: {
+    count: {
+      type: `count`
     },
-    dimensions: {
-      userId: {
-        sql: `user_id`,
-        type: `number`,
-        primaryKey: true
-      },
-      appsflyerId: {
-        sql: `appsflyer_id`,
-        type: `string`,
-      },
-      campaignId: {
-        sql: `${int_bloomreach_events_enhanced.campaign_id}`,
-        type: `string`
-      },
-      campaignPolicy: {
-        sql: `${int_bloomreach_events_enhanced.campaign_policy}`,
-        type: `string`
-      }
-      }
+    distinct_users: {
+      type: `countDistinct`,
+      sql: `${userId}`
+    }
+  },
+  dimensions: {
+    userId: {
+      sql: `user_id`,
+      type: `number`,
+      primaryKey: true
+    },
+    appsflyerId: {
+      sql: `appsflyer_id`,
+      type: `string`,
+    },
+    campaignId: {
+      sql: `${int_bloomreach_events_enhanced.campaign_id}`,
+      type: `string`
+    },
+    campaignPolicy: {
+      sql: `${int_bloomreach_events_enhanced.campaign_policy}`,
+      type: `string`
+    },
+    timestamp: {
+      sql: `${int_bloomreach_events_enhanced.timestamp}`,
+      type: `time`
+    }
+  }
 });
