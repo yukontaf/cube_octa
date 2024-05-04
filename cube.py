c6f9e6152a3952546391650da454661a6624b8ed
gsokolov <gsokolov@ourgapps.com>
2024-05-04 01:44:51 +0000

    same

--- cube.py
+++ cube.py
@@ -10,7 +10,7 @@ def semantic_layer_sync(ctx: dict) -> list[dict]:
         'api_token': 'f54ffebf-41bb-426b-906b-c6330ac66694',
         'api_secret': '952ef86a3b549084a637831c2d3612e134ce0275260dfd7c9fcf99cecf6ea897',
         'workspace_url': '20572de6.us1a.app.preset.io',
-        'database': 'Cube Cloud: octafx'
+        'database': 'Cube Cloud'
       }
     }
   ]
