0bb3f4d24c70e20bc44a485c1b7913c4305bce90
yukontaf <yukontaf@gmail.com>
2024-05-04 03:44:48 +0000

    experiment

--- model/globals.py
+++
@@ -1,9 +0,0 @@
-# Databricks notebook source
-from cube import TemplateContext
- 
-template = TemplateContext()
- 
-# Accessible from Jinja as 'get_data()'
-def get_data_1():
-  return 1
-template.add_function('get_data', get_data_1)
