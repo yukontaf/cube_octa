b222e1f5c87d49c6eb20501de2ee98a23849bfd5
Gleb Sokolov <yukontaf@gmail.com>
2024-05-04 04:16:57 +0300

    semantic layer sync modifications

--- cube.py
+++ cube.py
@@ -1,14 +1,16 @@
 from cube import config
 
 @config('semantic_layer_sync')
-def sls(ctx: dict) -> list:
-    return [{
-  "type": "preset",
-  "name": "Preset Sync",
-  "config": {
-    "database": "Cube Cloud: octafx",
-    "api_token": "f54ffebf-41bb-426b-906b-c6330ac66694",
-    "api_secret": "952ef86a3b549084a637831c2d3612e134ce0275260dfd7c9fcf99cecf6ea897",
-    "workspace_url": "20572de6.us1a.app.preset.io"
-  }
-}]
+def semantic_layer_sync(ctx: dict) -> list[dict]:
+  return [
+    {
+      'type': 'preset',
+      'name': "Preset Sync",
+      'config': {
+        'api_token': 'f54ffebf-41bb-426b-906b-c6330ac66694',
+        'api_secret': '952ef86a3b549084a637831c2d3612e134ce0275260dfd7c9fcf99cecf6ea897',
+        'workspace_url': '20572de6.us1a.app.preset.io',
+        'database': 'Cube Cloud: octafx'
+      }
+    }
+  ]
