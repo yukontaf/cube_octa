93272992c1555a051ec89c0db8de7890dff36112
gsokolov <gsokolov@ourgapps.com>
2024-05-05 19:26:23 +0000

    Batch update files

--- model/cubes/int_pushes.js
+++ model/cubes/int_pushes.js
@@ -1,34 +1,51 @@
 cube(`int_pushes`, {
   sql: `
-  SELECT
-      *
-      , CASE
-          WHEN event_number = 1 THEN timestamp
-      END AS first_push
-      , ROW_NUMBER() OVER (PARTITION BY user_id, CONCAT(campaign_id, '_', CAST(action_id AS STRING)) ORDER BY timestamp ASC) AS distinct_push_number
-  FROM (
-      SELECT *
-      FROM ${bloomreach_events.sql()}
+    SELECT
+      user_id,
+      campaign_id,
+      action_id,
+      event_number,
+      status,
+      first_push,
+      timestamp,
+      LEAD(timestamp) OVER (PARTITION BY user_id, campaign_id, action_id ORDER BY timestamp ASC) AS next_timestamp,
+      CASE
+        WHEN status = 'failed' AND LEAD(status) OVER (PARTITION BY user_id, campaign_id, action_id ORDER BY timestamp ASC) = 'delivered'
+        THEN TIMESTAMP_DIFF(LEAD(timestamp) OVER (PARTITION BY user_id, campaign_id, action_id ORDER BY timestamp ASC), timestamp, SECOND)
+        ELSE NULL
+      END AS timedelta
+    FROM (
+      SELECT
+        user_id,
+        campaign_id,
+        action_id,
+        TIMESTAMP as timestamp,
+        status,
+        ROW_NUMBER() OVER (PARTITION BY user_id, CONCAT(campaign_id, '_', CAST(action_id AS STRING)) ORDER BY timestamp) as event_number,
+        CASE WHEN event_number = 1 THEN timestamp END AS first_push
+      FROM bloomreach_events
       WHERE SAFE_CAST(user_id AS INT64) IN (
-              SELECT user_id
-              FROM ${eligible_users.sql()}
-          )
-          AND action_type = 'mobile notification'
-          AND DATE(timestamp) BETWEEN '2024-01-01' AND DATE(CURRENT_TIMESTAMP)
-          AND status IN ('delivered', 'failed')
-  )
+        SELECT user_id FROM eligible_users
+      )
+      AND action_type = 'mobile notification'
+      AND DATE(timestamp) BETWEEN '2024-01-01' AND CURRENT_DATE()
+      AND status IN ('delivered', 'failed')
+    ) detailed_events
   `,
 
   measures: {
-    count_user_id: {
+    countDelivered: {
+      sql: `COUNT(DISTINCT CASE WHEN status = 'delivered' THEN user_id ELSE NULL END)`,
       type: `count`,
-      sql: `user_id`,
     },
-    count_distinct_user_id: {
-      type: `count_distinct`,
-      sql: `user_id`,
+    countFailed: {
+      sql: `COUNT(DISTINCT CASE WHEN status = 'failed' THEN user_id ELSE NULL END)`,
+      type: `count`,
     },
-
+    avgTimeDelta: {
+      sql: `AVG(timedelta)`,
+      type: `avg`
+    }
   },
 
   dimensions: {
@@ -37,53 +54,29 @@ cube(`int_pushes`, {
       type: `number`,
       primaryKey: true,
     },
-    campaign_name: {
-      sql: `campaign_name`,
-      type: `string`,
-    },
-    first_push: {
-      sql: `first_push`,
-      type: `time`,
-    },
-    push_num: {
-      sql: `event_number`,
-      type: `number`,
-    },
-    distinct_push_num: {
-      sql: `distinct_push_number`,
-      type: `number`,
-    },
-    status: {
-      sql: `status`,
-      type: `string`,
-    },
-    status_num: {
-      sql: `CASE WHEN ${CUBE}.status='delivered' THEN 1 ELSE 0 END`,
-      type: `number`,
-    },
-    action_name: {
-      sql: `action_name`,
-      type: `string`,
+    campaign_id: {
+      sql: `campaign_id`,
+      type: `string`
     },
     action_id: {
       sql: `action_id`,
-      type: `number`,
+      type: `number`
+    },
+    status: {
+      sql: `status`,
+      type: `string`
     },
     timestamp: {
       sql: `timestamp`,
-      type: `time`,
+      type: `time`
     },
-    time_delta: {
-        sql: `${CUBE}.timestamp - LAG(${CUBE}.timestamp, 1) OVER (PARTITION BY ${CUBE}.user_id ORDER BY ${CUBE}.timestamp)`,
-        type: `time`
+    next_timestamp: {
+      sql: `next_timestamp`,
+      type: `time`
     },
-    delay: {
-        sql: `CASE
-                WHEN ${CUBE}.status = 'delivered' AND LAG(${CUBE}.status, 1) OVER (PARTITION BY ${CUBE}.user_id ORDER BY ${CUBE}.timestamp) = 'failed'
-                THEN ${CUBE}.timestamp - LAG(${CUBE}.timestamp, 1) OVER (PARTITION BY ${CUBE}.user_id ORDER BY ${CUBE}.timestamp)
-                ELSE NULL
-             END`,
-        type: `time`
-    },
+    timedelta: {
+      sql: `timedelta`,
+      type: `number`
+    }
   },
 });
