b406d1bc550033b165897e01d8e1b9d8feb26434
gsokolov <gsokolov@ourgapps.com>
2024-05-05 23:07:32 +0000

    Batch update files

--- model/cubes/int_pushes.js
+++ model/cubes/int_pushes.js
@@ -1,33 +1,6 @@
 cube(`int_pushes`, {
   sql: `
-    SELECT
-      user_id,
-      campaign_id,
-      action_id,
-      event_number,
-      status,
-      first_push,
-      timestamp,
-      event_id,
-      LEAD(timestamp) OVER (PARTITION BY user_id, campaign_id, action_id ORDER BY timestamp ASC) AS next_timestamp,
-    FROM (
-      SELECT
-        user_id,
-        campaign_id,
-        action_id,
-        TIMESTAMP as timestamp,
-        status,
-        ROW_NUMBER() OVER (PARTITION BY user_id, CONCAT(campaign_id, '_', CAST(action_id AS STRING)) ORDER BY timestamp) as event_number,
-        CASE WHEN event_number = 1 THEN timestamp END AS first_push,
-        event_id
-      FROM ${bloomreach_events.sql()}
-      WHERE SAFE_CAST(user_id AS INT64) IN (
-        SELECT user_id FROM ${eligible_users.sql()}
-      )
-      AND action_type = 'mobile notification'
-      AND DATE(timestamp) BETWEEN '2024-01-01' AND CURRENT_DATE()
-      AND status IN ('delivered', 'failed')
-    ) detailed_events
+    select * from dev_gsokolov.int_pushes
   `,
 
   measures: {
